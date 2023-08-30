2fed8f851ef8f05fea3b656d825f2a06f508d54e
stocaline <stocaline2@gmail.com>
2023-08-29 23:15:06 -0300

    Estrutura inicial da landing page

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+    smoothScrollLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
