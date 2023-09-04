7878e8fbe0685f7b9295d8f6a769119b13da9e03
RichardSB <richardbercheli@gmail.com>
2023-09-04 19:55:21 -0300

    nava estrutura

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
-    smoothScrollLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-});
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobileMenu = document.querySelector(".header .nav-bar .nav-list ul");
+const menuItem = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+const header = document.querySelector(".header.container")
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+})
+
+document.addEventListener('scroll', () => {
+    var scrollPosition = window.scrollY
+    if (scrollPosition > 250) {
+        header.style.backgroundColor = "#29323c"
+    } else[
+        header.style.backgroundColor = "transparent"
+    ]
+})
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active')
+        mobileMenu.classList.toggle('active')
+    })
+})
