948a9cfcec733189f1e62ef1352f2fe3c6bb9ae0
Richard de Souza Bercheli <60857052+stocaline@users.noreply.github.com>
2024-06-07 12:17:25 -0300

    Adicionando estrutura frontend

---
+++ frontend/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Lots</h1>
+      <Button>Hello</Button>
+    </main>
+  )
+}
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter"
+});
+
+export const metadata: Metadata = {
+  title: "Lots",
+  description: "Lots is a platform for manage your condominium",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
